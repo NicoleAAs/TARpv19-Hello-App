30b4a706fad83c0a1d2fafce77c01a27a3ec4fdd
NicoleAasakula <aasakyula@mail.ru>
2020-06-25 21:21:20 +0300

    Create HEllo App

---
+++ TARpv19/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TARpv19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
